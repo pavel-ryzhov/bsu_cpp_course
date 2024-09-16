63083970d6727f3589831072e869cd38fa4083a9
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-09-16 23:10:26 +0300

    Add bazel sum task

--- 00_test/sum_bazel/sum.h
+++ 00_test/sum_bazel/sum.h
@@ -1,15 +1,14 @@
 #pragma once
 
 #include <cstdint>
-#include <stdexcept>
 #include <string>
 
 inline int64_t Sum(int32_t a, int32_t b) {
-    throw std::runtime_error("Not implemented");
+    return static_cast<int64_t>(a) + b;
 }
 
 inline std::string BazelOutput() {
     // return output of "bazel query 'deps(//00_test/sum_bazel:all)' 2>/dev/null | grep -v "@" |
     // sort | md5sum | cut -c -15"
-    throw std::runtime_error("Not implemented");
+    return "8384be1f2a1f296";
 }
