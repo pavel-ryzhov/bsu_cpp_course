22051456ed5d09f10f8df8667d452bd93e1b4dda
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-09-30 22:09:05 +0300

    01_bin_pow

--- 01_first_tasks/bin-pow/bin_pow.h
+++ 01_first_tasks/bin-pow/bin_pow.h
@@ -1,8 +1,19 @@
 #pragma once
 
 #include <cstdint>
-#include <stdexcept>
+
+inline int32_t MultWithMod(int32_t a, int64_t b, int32_t c) {
+    return static_cast<int32_t>(((a % c) * (b % c)) % c);
+}
 
 inline int32_t BinPow(int32_t a, int64_t b, int32_t c) {
-    throw std::runtime_error("Not implemented");
+    int32_t result = 1;
+    while (b > 0) {
+        if (b % 2 == 1) {
+            result = MultWithMod(result, a, c);
+        } 
+        a = MultWithMod(a, a, c);
+        b /= 2;
+    }
+    return result % c;
 }
