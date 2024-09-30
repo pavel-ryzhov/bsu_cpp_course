10abc01154eb241c0db05b9d7708bc9fc9ed8057
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-09-30 22:22:58 +0300

    01_bin-pow

--- 01_first_tasks/bin-pow/bin_pow.h
+++ 01_first_tasks/bin-pow/bin_pow.h
@@ -11,7 +11,7 @@ inline int32_t BinPow(int32_t a, int64_t b, int32_t c) {
     while (b > 0) {
         if (b % 2 == 1) {
             result = MultWithMod(result, a, c);
-        } 
+        }
         a = MultWithMod(a, a, c);
         b /= 2;
     }
