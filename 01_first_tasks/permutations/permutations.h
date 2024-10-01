699e9afcb22590e6509e92233cda9ad04cec0e2a
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-02 00:15:07 +0300

    permutations

--- 01_first_tasks/permutations/permutations.h
+++ 01_first_tasks/permutations/permutations.h
@@ -1,8 +1,34 @@
 #pragma once
 
-#include <stdexcept>
+#include <cstddef>
+#include <set>
 #include <vector>
 
+inline std::vector<std::vector<int>> Gen(size_t len, std::set<int>& exclude) {
+    std::vector<std::vector<int>> result;
+    if (exclude.size() == len) {
+        return result;
+    }
+    for (int i = 0; i < static_cast<int>(len); i++) {
+        if (exclude.contains(i)) {
+            continue;
+        }
+        std::set excl(exclude);
+        excl.insert(i);
+        auto v = Gen(len, excl);
+        for (auto vect : v) {
+            std::vector vv(1, i);
+            vv.insert(vv.end(), vect.begin(), vect.end());
+            result.push_back(vv);
+        }
+        if (v.empty()) {
+            result.push_back({i});
+        }
+    }
+    return result;
+}
+
 inline std::vector<std::vector<int>> GeneratePermutations(size_t len) {
-    throw std::runtime_error("Not implemented");
+    std::set<int> s;
+    return Gen(len, s);
 }
