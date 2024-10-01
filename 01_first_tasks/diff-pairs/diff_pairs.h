0c208a96067becc3c2a5ae93e10b85798baef50a
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-01 21:19:16 +0300

    diff_pair

--- 01_first_tasks/diff-pairs/diff_pairs.h
+++ 01_first_tasks/diff-pairs/diff_pairs.h
@@ -1,8 +1,17 @@
 #pragma once
 
-#include <stdexcept>
+#include <cstddef>
+#include <unordered_map>
 #include <vector>
 
 inline size_t CountPairs(const std::vector<int>& data, int x) {
-    throw std::runtime_error("Not implemented");
+    std::unordered_map<int, size_t> map;
+    size_t count = 0;
+    for (const int d : data) {
+        if (map.find(d) != map.end()) {
+            count += map[d];
+        }
+        map[x - d]++;
+    }
+    return count;
 }
--- 01_first_tasks/diff-pairs/test_diff_pairs.cpp
+++ 01_first_tasks/diff-pairs/test_diff_pairs.cpp
@@ -3,5 +3,5 @@
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("Your test here") {
-    CountPairs({}, 0);
+    REQUIRE(11 == CountPairs({3, 3, 4, 8, 4, 5, 6, 9, 9, 3, 9}, 12));
 }
