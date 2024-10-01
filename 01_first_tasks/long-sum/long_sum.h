7526941ec0dd11053e1c0dc173e0fdf19d24cb99
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-01 22:08:36 +0300

    long_sum

--- 01_first_tasks/long-sum/long_sum.h
+++ 01_first_tasks/long-sum/long_sum.h
@@ -1,8 +1,25 @@
 #pragma once
 
-#include <stdexcept>
 #include <string>
+#include <algorithm>
 
 inline std::string LongSum(const std::string& a, const std::string& b) {
-    throw std::runtime_error("Not implemented");
+    std::string result;
+    std::string a1;
+    std::string b1;
+    a1.assign(a.rbegin(), a.rend());
+    b1.assign(b.rbegin(), b.rend());
+    bool r = false;
+    for (size_t i = 0; i < std::max(a.size(), b.size()); i++) {
+        const int val1 = i < a1.size() ? a1[i] - '0' : 0;
+        const int val2 = i < b1.size() ? b1[i] - '0' : 0;
+        const int num = val1 + val2 + static_cast<int>(r);
+        result += static_cast<char>(num % 10 + '0');
+        r = num > 9;
+    }
+    if (r) {
+        result += '1';
+    }
+    std::ranges::reverse(result);
+    return result;
 }
