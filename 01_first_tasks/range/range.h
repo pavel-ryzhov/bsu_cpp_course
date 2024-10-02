5cb71961b19882e8eef91c024e5f9dae38869397
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-02 18:25:00 +0300

    range

--- 01_first_tasks/range/range.h
+++ 01_first_tasks/range/range.h
@@ -1,7 +1,14 @@
 #pragma once
 
-#include <stdexcept>
+#include <cstdint>
+#include <vector>
 
 inline std::vector<int> Range(int from, int to, int step) {
-    throw std::runtime_error("Not implemented");
+    std::vector<int> v;
+    if (to > from == step > 0) {
+        for (int64_t i = from; step < 0 ? (i > to) : (i < to); i += step) {
+            v.push_back(static_cast<int>(i));
+        }
+    }
+    return v;
 }
