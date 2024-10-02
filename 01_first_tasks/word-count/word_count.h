4ed2673586e35e1699765d31d1ea59775d25301d
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-02 19:40:08 +0300

    word_count

--- 01_first_tasks/word-count/word_count.h
+++ 01_first_tasks/word-count/word_count.h
@@ -1,7 +1,24 @@
 #pragma once
 
-#include <stdexcept>
+#include <cctype>
+#include <cstddef>
+#include <string>
+#include <unordered_map>
 
 inline size_t DifferentWordsCount(const std::string& text) {
-    throw std::runtime_error("Not implemented");
+    std::unordered_map<std::string, size_t> map;
+    std::string word;
+    for (const char& c : text) {
+        if (static_cast<bool>(std::isalpha(c))) {
+            word += static_cast<char>(std::tolower(c));
+        } else if (!word.empty()) {
+            map[word]++;
+            word.clear();
+        }
+    }
+    if (!word.empty()) {
+        map[word]++;
+        word.clear();
+    }
+    return map.size();
 }
