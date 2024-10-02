4aeef3b1551283c3a6141cba9f507755fea9210e
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-02 19:13:25 +0300

    unique

--- 01_first_tasks/unique/unique.h
+++ 01_first_tasks/unique/unique.h
@@ -1,8 +1,18 @@
 #pragma once
 
-#include <stdexcept>
 #include <vector>
 
 inline std::vector<int> Unique(const std::vector<int>& data) {
-    throw std::runtime_error("Not implemented");
+    std::vector<int> result;
+    if (data.empty()) {
+        return result;
+    }
+    int previous = data[0] - 1;
+    for (const int d : data) {
+        if (d != previous) {
+            result.push_back(d);
+            previous = d;
+        }
+    }
+    return result;
 }
