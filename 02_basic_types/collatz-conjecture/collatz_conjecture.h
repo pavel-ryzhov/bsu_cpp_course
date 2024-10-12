3fe12eed562a6292d11dcf1abe1f22b8c8e929fa
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-12 18:31:50 +0300

    collatz-conjecture

--- 02_basic_types/collatz-conjecture/collatz_conjecture.h
+++ 02_basic_types/collatz-conjecture/collatz_conjecture.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 #include <optional>
 
 inline std::optional<int64_t> IterationsToConverge(int64_t n) {
-    throw std::runtime_error("Not implemented");
+    size_t counter = 0;
+    for (; counter < 100'000 && n != 1; counter++) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+    return n == 1 ? std::optional(counter) : std::nullopt;
 }
