711064e616ecf8b3b1a9ba34126755a88cac2f0f
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-12 19:02:50 +0300

    credit-card

--- 02_basic_types/credit-card/credit_card.h
+++ 02_basic_types/credit-card/credit_card.h
@@ -1,7 +1,22 @@
 #pragma once
 
-#include <stdexcept>
+#include <cstddef>
+#include <cstdint>
+#include <string>
 
 inline bool IsValidCreditCardNumber(int64_t n) {
-    throw std::runtime_error("Not implemented");
+    int64_t sum1 = 0;
+    int64_t sum2 = 0;
+    const size_t l = std::to_string(n).size();
+    for (size_t i = 0; i < l; i++) {
+        int64_t c = n % 10;
+        n /= 10;
+        if (i % 2 == 0) {
+            sum1 += c;
+        } else {
+            c *= 2;
+            sum2 += c % 10 + c / 10;
+        }
+    }
+    return (sum1 + sum2) % 10 == 0;
 }
