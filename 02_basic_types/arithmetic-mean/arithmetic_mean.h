7d0c82a356dadbad440d28b7bd0b20fee57c3237
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-12 14:20:09 +0300

    arithmetic-mean

--- 02_basic_types/arithmetic-mean/arithmetic_mean.h
+++ 02_basic_types/arithmetic-mean/arithmetic_mean.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <cmath>
 #include <cstdint>
-#include <stdexcept>
 
 inline int64_t ArithmeticMean(int64_t a, int64_t b) {
-    throw std::runtime_error("Not implemented");
+    const long double a1 = static_cast<long double>(a) / 2.0;
+    const long double b1 = static_cast<long double>(b) / 2.0;
+    return static_cast<int64_t>(a > b ? std::ceil(a1 + b1) : std::floor(a1 + b1));
 }
