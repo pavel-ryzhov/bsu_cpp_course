c5fe82ddb7c45cc180944d30c3f2300b4c97a7c8
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-13 00:10:37 +0300

    commit.h

--- 02_basic_types/split/split.h
+++ 02_basic_types/split/split.h
@@ -1,9 +1,21 @@
 #pragma once
 
-#include <stdexcept>
+#include <cstddef>
 #include <string>
 #include <vector>
 
 inline std::vector<std::string> Split(const std::string& string, const std::string& delimiter) {
-    throw std::runtime_error("Not implemented");
+    if (string.empty()) {
+        return {};
+    }
+    std::vector<std::string> result;
+    size_t pos = 0;
+    size_t p = 0;
+    while ((p = string.find(delimiter, p)) != std::string::npos) {
+        result.push_back(string.substr(pos, p - pos));
+        p += delimiter.size();
+        pos = p;
+    }
+    result.push_back(string.substr(pos));
+    return result;
 }
