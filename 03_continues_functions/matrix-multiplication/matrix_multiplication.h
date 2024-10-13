159d3d45c72730e1012d475cf5d7c37f53bbd6b7
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-13 21:09:19 +0300

    matrix-multiplication

--- 03_continues_functions/matrix-multiplication/matrix_multiplication.h
+++ 03_continues_functions/matrix-multiplication/matrix_multiplication.h
@@ -1,10 +1,23 @@
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
-#include <stdexcept>
 #include <vector>
 
 inline std::vector<std::vector<int32_t>> MultiplyMatrices(
     const std::vector<std::vector<int32_t>>& lhs, const std::vector<std::vector<int32_t>>& rhs) {
-    throw std::runtime_error("Not implemented");
+    const size_t l = lhs.size();
+    const size_t m = rhs.size();
+    const size_t n = rhs.front().size();
+    std::vector result(l, std::vector<int32_t>());
+    for (size_t i = 0; i < l; i++) {
+        for (size_t j = 0; j < n; j++) {
+            int32_t a = 0;
+            for (size_t r = 0; r < m; r++) {
+                a += lhs[i][r] * rhs[r][j];
+            }
+            result[i].push_back(a);
+        }
+    }
+    return result;
 }
