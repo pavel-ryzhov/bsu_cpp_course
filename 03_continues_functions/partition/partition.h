b4d81012c46734bc17c322c907e5ba22e4cb0d0c
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-13 22:05:28 +0300

    partition

--- 03_continues_functions/partition/partition.h
+++ 03_continues_functions/partition/partition.h
@@ -1,8 +1,26 @@
 #pragma once
 
-#include <stdexcept>
+#include <algorithm>
+#include <cstddef>
 #include <vector>
 
-inline void PartitionBySign(std::vector<int>& /*array*/) {
-    throw std::runtime_error("Not implemented");
+inline void PartitionBySign(std::vector<int>& array) {
+    const size_t size = array.size();
+    size_t index = 0;
+    for (size_t i = 0; i < size; i++) {
+        if (array[i] < 0) {
+            for (size_t j = i; j > index; j--) {
+                std::swap(array[j], array[j - 1]);
+            }
+            index++;
+        }
+    }
+    for (size_t i = 0; i < size; i++) {
+        if (array[i] == 0) {
+            for (size_t j = i; j > index; j--) {
+                std::swap(array[j], array[j - 1]);
+            }
+            index++;
+        }
+    }
 }
