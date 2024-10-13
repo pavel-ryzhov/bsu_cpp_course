f7c5e7fc55553af5db75ba9efb85b5347265e28a
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-13 22:20:33 +0300

    permutation

--- 03_continues_functions/permutation/permutation.h
+++ 03_continues_functions/permutation/permutation.h
@@ -1,7 +1,15 @@
 #pragma once
 
-#include <stdexcept>
+#include <algorithm>
+#include <vector>
 
 inline bool IsPermutation(const std::vector<int>& array) {
-    throw std::runtime_error("Not implemented");
+    unsigned int result = 0;
+    unsigned int max = 0;
+    for (unsigned int i = 0; i < array.size(); i++) {
+        const unsigned int d = array[i];
+        max = std::max(max, d);
+        result ^= d ^ (i + 1);
+    }
+    return result == 0 && array.size() == max;
 }
