ededc0b5f0eb4fece7eb0397188bd3e789d69a9a
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-13 20:08:14 +0300

    digit

--- 03_continues_functions/least-missing-digit/least_missing_digit.h
+++ 03_continues_functions/least-missing-digit/least_missing_digit.h
@@ -1,7 +1,17 @@
 #pragma once
 
-#include <stdexcept>
+#include <set>
 
 inline int LeastMissingDigit(int n) {
-    throw std::runtime_error("Not implemented");
+    std::set<int> digits;
+    while (n > 0) {
+        digits.insert(n % 10);
+        n /= 10;
+    }
+    for (int i = 1; i < 10; i++) {
+        if (!digits.contains(i)) {
+            return i;
+        }
+    }
+    return -1;
 }
