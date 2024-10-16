2ac19b59aee6170a1beea5f7798127cdfe08efbb
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-16 13:46:41 +0300

    reverse-array

--- 03_continues_functions/reverse-array/reverse_array.h
+++ 03_continues_functions/reverse-array/reverse_array.h
@@ -1,8 +1,11 @@
 #pragma once
 
-#include <stdexcept>
+#include <cstddef>
+#include <utility>
 #include <vector>
 
 inline void ReverseArray(std::vector<int>& array) {
-    throw std::runtime_error("Not implemented");
+    for (size_t i = 0; i < array.size() / 2; i++) {
+        std::swap(array[i], array[array.size() - i - 1]);
+    }
 }
