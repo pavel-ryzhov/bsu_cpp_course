bd66b8af7fa2bdbac0216e0f84e38fbbd1032cd8
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-17 17:45:34 +0300

    sort-studentd

--- 04_structs/sort-students/sort_students.h
+++ 04_structs/sort-students/sort_students.h
@@ -3,11 +3,35 @@
 #include <algorithm>
 #include <cstdint>
 #include <string>
+#include <utility>
 #include <vector>
 
 struct Student {
-    std::string name, surname;
-    int year, month, day;
+    Student(std::string name, std::string surname, int year, int month, int day): name_(std::move(name)), surname_(std::move(surname)), year_(year), month_(month), day_(day) {}
+
+    [[nodiscard]] std::string Name() const {
+        return name_;
+    }
+
+    [[nodiscard]] std::string Surname() const {
+        return surname_;
+    }
+
+    [[nodiscard]] int Year() const {
+        return year_;
+    }
+
+    [[nodiscard]] int Month() const {
+        return month_;
+    }
+
+    [[nodiscard]] int Day() const {
+        return day_;
+    }
+
+private:
+    std::string name_, surname_;
+    int year_, month_, day_;
 };
 
 enum class SortType : uint8_t { kByName, kByDate };
@@ -16,9 +40,9 @@ inline void SortStudents(std::vector<Student>* students, SortType sort_type) {
     std::sort(students->begin(), students->end(), [&](const Student& a, const Student& b) {
         switch (sort_type) {
             case SortType::kByName:
-                return a.name < b.name;
+                return a.Name() < b.Name();
             case SortType::kByDate:
-                return a.year < b.year || a.month < b.month || a.day < b.day;
+                return a.Year() < b.Year() || a.Month() < b.Month() || a.Day() < b.Day();
             default:
                 return false;
         }
