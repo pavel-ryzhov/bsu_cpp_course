009e1fd533ff49f7e6440cca30c083825dddde22
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-21 16:16:27 +0300

    sort_students.h

--- 04_structs/sort-students/sort_students.h
+++ 04_structs/sort-students/sort_students.h
@@ -12,13 +12,33 @@ struct Student {
 
 enum class SortType : uint8_t { kByName, kByDate };
 
+bool CompareStudents(const Student& a, const Student& b);
+
+bool CompareStudentsByName(const Student& a, const Student& b);
+
+inline bool Equal(const Student& a, const Student& b) {
+    return a.name == b.name && a.surname == b.surname && a.year == b.year && a.month == b.month &&
+           a.day == b.day;
+}
+
+inline bool CompareStudents(const Student& a, const Student& b) {
+    return a.year < b.year || a.month < b.month || a.day < b.day || CompareStudentsByName(a, b);
+}
+
+inline bool CompareStudentsByName(const Student& a, const Student& b) {
+    return a.surname.compare(b.surname) < 0 || a.name.compare(b.name) < 0 || CompareStudents(a, b);
+}
+
 inline void SortStudents(std::vector<Student>* students, SortType sort_type) {
     std::ranges::sort(students->begin(), students->end(), [&](const Student& a, const Student& b) {
+        if (Equal(a, b)) {
+            return false;
+        }
         switch (sort_type) {
             case SortType::kByName:
-                return a.name.compare(b.name) < 0 || a.surname.compare(b.surname) < 0;
+                return CompareStudentsByName(a, b);
             case SortType::kByDate:
-                return a.year < b.year || a.month < b.month || a.day < b.day;
+                return CompareStudents(a, b);
             default:
                 return false;
         }
