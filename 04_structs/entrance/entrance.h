d80d3d729a552d5304d9eebd15f586e21cac84b4
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-24 22:36:13 +0300

    entrance.h

--- 04_structs/entrance/entrance.h
+++ 04_structs/entrance/entrance.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <algorithm>
+#include <cstddef>
 #include <map>
-#include <stdexcept>
 #include <string>
 #include <tuple>
+#include <utility>
 #include <vector>
 
 struct StudentName {
@@ -11,12 +13,39 @@ struct StudentName {
 };
 
 struct Date {
-    int year, month, day;
+    int year = 0, month = 0, day = 0;
 };
+
+inline void SortStudents(
+    std::vector<std::tuple<StudentName, Date, int, std::vector<std::string>>>& students) {
+    std::ranges::sort(
+        students, [&](const std::tuple<StudentName, Date, int, std::vector<std::string>>& a,
+                      const std::tuple<StudentName, Date, int, std::vector<std::string>>& b) {
+            const auto& [name1, date1, score1, universities1] = a;
+            const auto& [name2, date2, score2, universities2] = b;
+            return std::tie(score1, date1.year, date1.month, date1.day, name1.surname, name1.name) <
+                   std::tie(score2, date2.year, date2.month, date2.day, name2.surname, name2.name);
+        });
+}
 
 inline std::map<std::string, std::vector<StudentName>> GetStudents(
     const std::vector<std::pair<std::string, int>>& universities_info,
     const std::vector<std::tuple<StudentName, Date, int, std::vector<std::string>>>&
         students_info) {
-    throw std::runtime_error{"Not implemented!"};
+    std::map universities0(universities_info.begin(), universities_info.end());
+    std::vector students(students_info);
+    std::map<std::string, std::vector<StudentName>> result;
+    for (const auto& university : universities_info) {
+        result[university.first] = std::vector<StudentName>();
+    }
+    SortStudents(students);
+    for (const auto& [name, date, score, universities] : students) {
+        for (const auto& university : universities) {
+            if (result[university].size() < static_cast<size_t>(universities0[university])) {
+                result[university].push_back(name);
+                break;
+            }
+        }
+    }
+    return result;
 }
