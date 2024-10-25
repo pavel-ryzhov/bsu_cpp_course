4171de67eed36c1b87cb1088a764bf64a316f381
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-25 15:49:37 +0300

    point_triangle.h

--- 04_structs/point-triangle/point_triangle.h
+++ 04_structs/point-triangle/point_triangle.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 struct Point {
     int x, y;
 };
@@ -8,16 +10,16 @@ struct Triangle {
     Point a, b, c;
 };
 
-inline int Multiply(int x1, int y1, int x2, int y2) {
+inline int64_t Multiply(int64_t x1, int64_t y1, int64_t x2, int64_t y2) {
     return (x1 * y2) - (y1 * x2);
 }
 
 inline bool IsPointInTriangle(const Triangle& t, const Point& pt) {
-    const int r1 = Multiply(pt.x - t.a.x, pt.y - t.a.y, t.b.x - t.a.x, t.b.y - t.a.y);
-    const int r2 = Multiply(pt.x - t.b.x, pt.y - t.b.y, t.c.x - t.b.x, t.c.y - t.b.y);
-    const int r3 = Multiply(pt.x - t.c.x, pt.y - t.c.y, t.a.x - t.c.x, t.a.y - t.c.y);
+    const int64_t r1 = Multiply(pt.x - t.a.x, pt.y - t.a.y, t.b.x - t.a.x, t.b.y - t.a.y);
+    const int64_t r2 = Multiply(pt.x - t.b.x, pt.y - t.b.y, t.c.x - t.b.x, t.c.y - t.b.y);
+    const int64_t r3 = Multiply(pt.x - t.c.x, pt.y - t.c.y, t.a.x - t.c.x, t.a.y - t.c.y);
     if (r1 == 0 || r2 == 0 || r3 == 0) {
-        return true;
+        return false;
     }
     return (r1 > 0 && r2 > 0 && r3 > 0) || (r1 < 0 && r2 < 0 && r3 < 0);
 }
