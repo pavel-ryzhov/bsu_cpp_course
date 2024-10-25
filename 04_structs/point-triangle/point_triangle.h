5abeba68343e2c29da05210da30a6ba292c0bc71
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-25 15:22:06 +0300

    point-triangle

--- 04_structs/point-triangle/point_triangle.h
+++ 04_structs/point-triangle/point_triangle.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <stdexcept>
-
 struct Point {
     int x, y;
 };
@@ -10,6 +8,16 @@ struct Triangle {
     Point a, b, c;
 };
 
+inline int Multiply(int x1, int y1, int x2, int y2) {
+    return (x1 * y2) - (y1 * x2);
+}
+
 inline bool IsPointInTriangle(const Triangle& t, const Point& pt) {
-    throw std::runtime_error{"Not implemented!"};
+    const int r1 = Multiply(pt.x - t.a.x, pt.y - t.a.y, t.b.x - t.a.x, t.b.y - t.a.y);
+    const int r2 = Multiply(pt.x - t.b.x, pt.y - t.b.y, t.c.x - t.b.x, t.c.y - t.b.y);
+    const int r3 = Multiply(pt.x - t.c.x, pt.y - t.c.y, t.a.x - t.c.x, t.a.y - t.c.y);
+    if (r1 == 0 || r2 == 0 || r3 == 0) {
+        return false;
+    }
+    return (r1 > 0 && r2 > 0 && r3 > 0) || (r1 < 0 && r2 < 0 && r3 < 0);
 }
