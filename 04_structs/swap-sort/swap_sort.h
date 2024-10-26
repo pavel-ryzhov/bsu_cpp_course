3047bdc948c7de2b4d9d99c6e032406f210ee0e3
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-26 21:32:16 +0300

    swap_sort.h

--- 04_structs/swap-sort/swap_sort.h
+++ 04_structs/swap-sort/swap_sort.h
@@ -1,11 +1,19 @@
 #pragma once
 
-#include <stdexcept>
-
 inline void Swap(int* a, int* b) {
-    throw std::runtime_error{"Not implemented!"};
+    const int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 inline void Sort3(int* a, int* b, int* c) {
-    throw std::runtime_error{"Not implemented!"};
+    if (*c < *a) {
+        Swap(a, c);
+    }
+    if (*c < *b) {
+        Swap(b, c);
+    }
+    if (*b < *a) {
+        Swap(a, b);
+    }
 }
