302c367461481538eda3eb6bdae97bd1c3d78781
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-26 21:13:39 +0300

    quadratic.h

--- 04_structs/quadratic/quadratic.h
+++ 04_structs/quadratic/quadratic.h
@@ -1,8 +1,10 @@
 #pragma once
 
-#include <stdexcept>
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
 
-enum class RootCount { kZero, kOne, kTwo, kInf };
+enum class RootCount : uint8_t { kZero, kOne, kTwo, kInf };
 
 struct Roots {
     RootCount count;
@@ -11,5 +13,23 @@ struct Roots {
 };
 
 inline Roots SolveQuadratic(int a, int b, int c) {
-    throw std::runtime_error{"Not implemented!"};
+    if (a == 0 && b == 0 && c == 0) [[unlikely]] {
+        return Roots{RootCount::kInf};
+    }
+    if (a == 0 && b == 0) {
+        return Roots{RootCount::kZero};
+    }
+    if (a == 0) {
+        return Roots{RootCount::kOne, static_cast<double>(-c) / b};
+    }
+    const int d = (b * b) - (4 * a * c);
+    if (d < 0) {
+        return Roots{RootCount::kZero};
+    }
+    if (d == 0) {
+        return Roots{RootCount::kOne, -b / (2.0 * a)};
+    }
+    const double r1 = (-b - std::sqrt(d)) / (2.0 * a);
+    const double r2 = (-b + std::sqrt(d)) / (2.0 * a);
+    return Roots{RootCount::kTwo, std::min(r1, r2), std::max(r1, r2)};
 }
