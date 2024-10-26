56a249ee08e6e9794ff7e0f0394ab662de3864bc
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-26 20:15:18 +0300

    factorization.h

--- 04_structs/factorization/factorization.h
+++ 04_structs/factorization/factorization.h
@@ -1,9 +1,26 @@
 #pragma once
 
-#include <stdexcept>
+#include <cmath>
+#include <cstdint>
+#include <map>
 #include <utility>
 #include <vector>
 
 inline std::vector<std::pair<int64_t, int>> Factorize(int64_t x) {
-    throw std::runtime_error{"Not implemented!"};
+    std::map<int64_t, int> result;
+    for (int64_t i = 2; i <= static_cast<int64_t>(std::sqrt(x)); i++) {
+        while (x % i == 0) {
+            result[i]++;
+            x /= i;
+        }
+    }
+    if (x != 1) {
+        result[x]++;
+    }
+    auto v = std::vector<std::pair<int64_t, int>>();
+    v.reserve(result.size());
+    for (const auto& p : result) {
+        v.emplace_back(p);
+    }
+    return v;
 }
