f408c0a5717ce9bdd9ea06325ff3a617e811f443
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-27 22:27:51 +0300

    frozen_map.h

--- 05_classes_begin/frozen-map/frozen_map.h
+++ 05_classes_begin/frozen-map/frozen_map.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <string>
 #include <utility>
 #include <vector>
@@ -7,9 +8,26 @@
 class FrozenMap {
    public:
     explicit FrozenMap(const std::vector<std::pair<std::string, std::string>>& items) {
+        items_.reserve(items.size());
+        items_.insert(items_.end(), items.begin(), items.end());
+        std::sort(
+            items_.begin(), items_.end(),
+            [](const std::pair<std::string, std::string>& a,
+               const std::pair<std::string, std::string>& b) { return a.first < b.first; });
     }
 
     bool Find(const std::string& key, std::string* value) const {
-        return false;
+        const auto a = std::lower_bound(
+            items_.begin(), items_.end(), std::pair(key, ""),
+            [](const std::pair<std::string, std::string>& a,
+               const std::pair<std::string, std::string>& b) { return a.first < b.first; });
+        if (a == items_.end() || a->first != key) {
+            return false;
+        }
+        *value = a->second;
+        return true;
     }
+
+   private:
+    std::vector<std::pair<std::string, std::string>> items_;
 };
