e3879b9f38e10a0b37aa6fe2a0316e32e4a3d06d
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-10-28 16:01:16 +0300

    ring_buffer.h

--- 05_classes_begin/ring-buffer/ring_buffer.h
+++ 05_classes_begin/ring-buffer/ring_buffer.h
@@ -1,25 +1,47 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
 
 class RingBuffer {
    public:
-    explicit RingBuffer(size_t capacity) {
+    explicit RingBuffer(size_t capacity) : capacity_(capacity) {
+        data_.resize(capacity);
     }
 
-    size_t Size() const {
-        return 0;
+    [[nodiscard]] size_t Size() const {
+        return size_;
     }
 
-    bool Empty() const {
-        return false;
+    [[nodiscard]] bool Empty() const {
+        return size_ == 0;
     }
 
     bool TryPush(int element) {
+        if (size_ == capacity_) {
+            return false;
+        }
+        data_[begin_] = element;
+        size_++;
+        begin_ + 1 < capacity_ ? begin_++ : begin_ = 0;
         return true;
     }
 
     bool TryPop(int* element) {
+        if (size_ == 0) {
+            return false;
+        }
+        *element = data_[end_];
+        data_[end_] = 0;
+        size_--;
+        end_ + 1 < capacity_ ? end_++ : end_ = 0;
         return true;
     }
+
+   private:
+    size_t size_{0};
+    size_t capacity_;
+    size_t begin_{0};
+    size_t end_{0};
+    std::vector<int> data_;
 };
