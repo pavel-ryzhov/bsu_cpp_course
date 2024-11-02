db6f6b59f716182d8f8e4d87ad66e5c11f73129c
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-11-03 01:07:50 +0300

    images

--- 05_classes_begin/images/image.h
+++ 05_classes_begin/images/image.h
@@ -1,41 +1,119 @@
 #pragma once
+
 #include "matrix.h"
 #include "tools/util/rgb.h"
 
 #include <algorithm>
-#include <cassert>
 #include <cstddef>
 #include <cstdint>
-#include <stdexcept>
 
 using MatrixImage = Matrix<RGB>;
 using RGBImage = Matrix<RGB>;
 using GrayscaleImage = Matrix<uint8_t>;
 
 template <class T>
-inline Matrix<T> FlipHorizontally(const Matrix<T>&) {
-    throw std::runtime_error{"Not implemented"};
+Matrix<T> FlipHorizontally(const Matrix<T>& matrix) {
+    Matrix<T> result = matrix;
+    for (size_t i = 0; i < result.Rows(); i++) {
+        for (size_t j = 0; j < result.Columns() / 2; j++) {
+            std::swap(result(i, j), result(i, result.Columns() - j - 1));
+        }
+    }
+    return result;
 }
 
-inline GrayscaleImage ToGray(const RGBImage&) {
-    throw std::runtime_error{"Not implemented"};
+inline GrayscaleImage ToGray(const RGBImage& matrix) {
+    GrayscaleImage result(matrix.Rows(), matrix.Columns());
+    for (size_t i = 0; i < matrix.Rows(); i++) {
+        for (size_t j = 0; j < matrix.Columns(); j++) {
+            const auto color = matrix(i, j);
+            result(i, j) = (color.r + color.g + color.b) / 3;
+        }
+    }
+    return result;
 }
 
-inline RGBImage Blend(const RGBImage&, const RGBImage&) {
-    throw std::runtime_error{"Not implemented"};
+inline uint8_t BlendComponent(uint8_t a, uint8_t b, float alpha) {
+    return static_cast<uint8_t>(
+        (static_cast<float>(b) * alpha) + (static_cast<float>(a) * (1 - alpha)));
+}
+
+inline RGB BlendSum(const RGB a, const RGB b, const float alpha) {
+    const uint8_t red = BlendComponent(a.r, b.r, alpha);
+    const uint8_t green = BlendComponent(a.g, b.g, alpha);
+    const uint8_t blue = BlendComponent(a.b, b.b, alpha);
+    return RGB(red, green, blue);
+}
+
+inline RGBImage Blend(const RGBImage& left, const RGBImage& right) {
+    RGBImage result(left.Rows(), left.Columns());
+    for (size_t i = 0; i < left.Columns(); i++) {
+        const float alpha = static_cast<float>(i) / static_cast<float>(left.Columns() - 1);
+        for (size_t j = 0; j < left.Rows(); j++) {
+            result(j, i) = BlendSum(left(j, i), right(j, i), alpha);
+        }
+    }
+    return result;
 }
 
 template <class T>
-inline Matrix<T> RotateClockwise(const Matrix<T>&) {
-    throw std::runtime_error{"Not implemented"};
+Matrix<T> RotateClockwise(const Matrix<T>& matrix) {
+    return FlipHorizontally(matrix.Transpose());
+}
+
+inline uint8_t FloatColorToUInt8T(float color) {
+    if (color > 255) {
+        return 255;
+    }
+    if (color < 0) {
+        return 0;
+    }
+    return static_cast<uint8_t>(color);
 }
 
 inline RGBImage MixChannels(
     [[maybe_unused]] const RGBImage& image, [[maybe_unused]] const Matrix<float>& filter) {
-    throw std::runtime_error{"Not implemented"};
+    RGBImage result(image.Rows(), image.Columns());
+    for (size_t i = 0; i < result.Rows(); i++) {
+        for (size_t j = 0; j < result.Columns(); j++) {
+            const RGB rgb = image(i, j);
+            const Matrix<float> color(
+                {{static_cast<float>(rgb.r), static_cast<float>(rgb.g),
+                  static_cast<float>(rgb.b)}});
+            const Matrix<float> result_color = (filter * color.Transpose()).Transpose();
+            auto red = FloatColorToUInt8T(result_color.Data().front()[0]);
+            auto green = FloatColorToUInt8T(result_color.Data().front()[1]);
+            auto blue = FloatColorToUInt8T(result_color.Data().front()[2]);
+            result(i, j) = RGB(red, green, blue);
+        }
+    }
+    return result;
 }
 
 inline RGBImage Convolve(
     [[maybe_unused]] const RGBImage& image, [[maybe_unused]] const Matrix<float>& kernel) {
-    throw std::runtime_error{"Not implemented"};
+    RGBImage result(image.Rows() - kernel.Rows() + 1, image.Columns() - kernel.Columns() + 1);
+    const size_t kernel_half_rows = kernel.Rows() / 2;
+    const size_t kernel_half_columns = kernel.Columns() / 2;
+    for (size_t y = kernel_half_rows; y < image.Rows() - kernel_half_rows; y++) {
+        for (size_t x = kernel_half_columns; x < image.Columns() - kernel_half_columns; x++) {
+            float sum_r = 0;
+            float sum_g = 0;
+            float sum_b = 0;
+            for (size_t i = 0; i < kernel.Rows(); i++) {
+                for (size_t j = 0; j < kernel.Columns(); j++) {
+                    const auto k = kernel(i, j);
+                    const auto [r, g, b] =
+                        image(x - kernel_half_rows + i, y - kernel_half_columns + j);
+                    sum_r += k * static_cast<float>(r);
+                    sum_g += k * static_cast<float>(g);
+                    sum_b += k * static_cast<float>(b);
+                    result(y - kernel_half_rows, x - kernel_half_columns) =
+                        RGB(FloatColorToUInt8T(sum_r), FloatColorToUInt8T(sum_g),
+                            FloatColorToUInt8T(sum_b));
+                }
+            }
+        }
+    }
+    return result;
 }
--- 05_classes_begin/images/matrix.h
+++ 05_classes_begin/images/matrix.h
@@ -1,19 +1,164 @@
 #pragma once
 
+#include <cstddef>
 #include <stdexcept>
 #include <vector>
 
 template <class T>
 class Matrix {
    public:
-    [[nodiscard]] size_t Rows() const;
+    static Matrix Identity(size_t n) {
+        Matrix matrix(n, n, static_cast<T>(0));
+        for (size_t i = 0; i < n; i++) {
+            matrix(i, i) = static_cast<T>(1);
+        }
+        return matrix;
+    }
 
-    [[nodiscard]] size_t Columns() const;
+    Matrix(size_t n, size_t m, const T& value = T()) : rows_(n), columns_(m) {
+        data_.resize(n, std::move(std::vector<T>(m, value)));
+    }
 
-    const std::vector<std::vector<T>>& Data() const;
+    explicit Matrix(size_t n) : Matrix(n, n, T()) {
+    }
+
+    explicit Matrix(std::vector<std::vector<T> > matrix) {
+        if (matrix.empty() || matrix.front().empty()) {
+            throw std::invalid_argument("Empty matrix!");
+        }
+        const size_t columns = matrix.front().size();
+        for (size_t i = 1; i < matrix.size(); i++) {
+            if (matrix[i].size() != columns) {
+                throw std::invalid_argument("Not a matrix!");
+            }
+        }
+        columns_ = columns;
+        rows_ = matrix.size();
+        data_ = std::move(matrix);
+    }
+
+    Matrix(const Matrix& matrix) = default;
+
+    Matrix(Matrix&& matrix) noexcept = default;
+
+    Matrix& operator=(const Matrix& matrix) = default;
+
+    Matrix& operator=(Matrix&& matrix) noexcept = default;
+
+    ~Matrix() = default;
+
+    const T& operator()(size_t i, size_t j) const {
+        CheckCoords(i, j);
+        return data_[i][j];
+    }
+
+    T& operator()(size_t i, size_t j) {
+        CheckCoords(i, j);
+        return data_[i][j];
+    }
+
+    Matrix& operator+=(const Matrix& matrix) {
+        ForEach([&](size_t i, size_t j) { (*this)(i, j) += matrix(i, j); });
+        return *this;
+    }
+
+    Matrix& operator-=(const Matrix& matrix) {
+        ForEach([&](size_t i, size_t j) { (*this)(i, j) -= matrix(i, j); });
+        return *this;
+    }
+
+    Matrix operator+(const Matrix& matrix) const {
+        Matrix m = *this;
+        m += matrix;
+        return m;
+    }
+
+    Matrix operator-(const Matrix& matrix) const {
+        Matrix m = *this;
+        m -= matrix;
+        return m;
+    }
+
+    Matrix& operator*=(const Matrix& matrix) {
+        *this = *this * matrix;
+        return *this;
+    }
+
+    Matrix operator*(const Matrix& matrix) const {
+        const size_t l = this->rows_;
+        const size_t m = matrix.rows_;
+        const size_t n = matrix.columns_;
+        Matrix result(l, n);
+        for (size_t i = 0; i < l; i++) {
+            for (size_t j = 0; j < n; j++) {
+                T a = 0;
+                for (size_t r = 0; r < m; r++) {
+                    a += (*this)(i, r) * matrix(r, j);
+                }
+                result(i, j) = a;
+            }
+        }
+        return result;
+    }
+
+    bool operator==(const Matrix& matrix) const {
+        if (this->rows_ != matrix.rows_ || this->columns_ != matrix.columns_) {
+            return false;
+        }
+        for (size_t i = 0; i < this->rows_; i++) {
+            for (size_t j = 0; j < this->columns_; j++) {
+                if ((*this)(i, j) != matrix(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool operator!=(const Matrix& matrix) const {
+        return !(*this == matrix);
+    }
+
+    [[nodiscard]] Matrix Transpose() const {
+        Matrix transposed(columns_, rows_);
+        ForEach([&](size_t i, size_t j) { transposed(j, i) = (*this)(i, j); });
+        return transposed;
+    }
+
+    [[nodiscard]] size_t Rows() const {
+        return rows_;
+    }
+
+    [[nodiscard]] size_t Columns() const {
+        return columns_;
+    }
+
+    [[nodiscard]] const std::vector<std::vector<T> >& Data() const {
+        return data_;
+    }
+
+   private:
+    std::vector<std::vector<T> > data_;
+    size_t rows_;
+    size_t columns_;
+
+    void CheckCoords(size_t i, size_t j) const {
+        if (i >= rows_ || j >= columns_) {
+            throw std::invalid_argument("Coordinates out of range!");
+        }
+    }
+
+    template <typename Function>
+    void ForEach(Function function) const {
+        for (size_t i = 0; i < rows_; i++) {
+            for (size_t j = 0; j < columns_; j++) {
+                function(i, j);
+            }
+        }
+    }
 };
 
 template <class T>
-inline Matrix<T> Transpose(const Matrix<T>& /*unused*/) {
-    throw std::runtime_error{"Not implemented"};
+Matrix<T> Transpose(const Matrix<T>& matrix) {
+    return matrix.Transpose();
 }
