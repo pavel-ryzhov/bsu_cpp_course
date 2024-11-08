a9359e51cabcd56375ccaad26d17fc0f40da1463
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-11-08 16:10:18 +0300

    functors.h

--- 06_templates/functors/functors.h
+++ 06_templates/functors/functors.h
@@ -1,12 +1,35 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
 
 template <class Functor>
-class ReverseUnaryFunctor {};
+class ReverseUnaryFunctor {
+   public:
+    explicit ReverseUnaryFunctor(Functor functor) : functor_(functor) {
+    }
+
+    bool operator()(auto arg) {
+        return !functor_(arg);
+    }
+
+   private:
+    Functor functor_;
+};
 
 template <class Functor>
-class ReverseBinaryFunctor {};
+class ReverseBinaryFunctor {
+   public:
+    explicit ReverseBinaryFunctor(Functor functor) : functor_(functor) {
+    }
+
+    bool operator()(auto arg1, auto arg2) {
+        return !functor_(arg1, arg2);
+    }
+
+   private:
+    Functor functor_;
+};
 
 auto MakeReverseUnaryFunctor(auto functor) {
     return ReverseUnaryFunctor{functor};
@@ -17,4 +40,11 @@ auto MakeReverseBinaryFunctor(auto functor) {
 }
 
 template <class Iterator>
-size_t ComparisonsCount(Iterator first, Iterator last);
+size_t ComparisonsCount(Iterator first, Iterator last) {
+    size_t count = 0;
+    std::sort(first, last, [&count](auto a, auto b) {
+        count++;
+        return a < b;
+    });
+    return count;
+}
