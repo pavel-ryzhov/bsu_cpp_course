faccbfcf05e19867e2e5effd0a277d32a1daf8c6
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-11-14 11:42:29 +0300

    dedup.h

--- 10_classes_smart_ptrs/dedup/dedup.h
+++ 10_classes_smart_ptrs/dedup/dedup.h
@@ -1,10 +1,29 @@
 #pragma once
 
 #include <memory>
+#include <unordered_map>
 #include <vector>
 
 template <class T>
-std::vector<std::unique_ptr<T>> Duplicate(const std::vector<std::shared_ptr<T>>& items);
+std::vector<std::unique_ptr<T>> Duplicate(const std::vector<std::shared_ptr<T>>& items) {
+    std::vector<std::unique_ptr<T>> result;
+    result.reserve(items.size());
+    for (const auto& item : items) {
+        result.push_back(std::make_unique<T>(*item));
+    }
+    return result;
+}
 
 template <class T>
-std::vector<std::shared_ptr<T>> DeDuplicate(const std::vector<std::unique_ptr<T>>& items);
+std::vector<std::shared_ptr<T>> DeDuplicate(const std::vector<std::unique_ptr<T>>& items) {
+    std::vector<std::shared_ptr<T>> result;
+    std::unordered_map<T, std::shared_ptr<T>> map;
+    result.reserve(items.size());
+    for (const auto& item : items) {
+        if (!map.contains(*item)) {
+            map[*item] = std::make_shared<T>(*item);
+        }
+        result.push_back(map[*item]);
+    }
+    return result;
+}
