ad4e7826c650d80719906566b58e0c0fae97dccc
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-11-25 23:30:38 +0300

    safe_transform.h

--- 11_inheritance/safe-transform/safe_transform.h
+++ 11_inheritance/safe-transform/safe_transform.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <iterator>
+#include <utility>
+#include <vector>
+
+template <class Iterator, class Predicate, class Functor>
+void TransformIf(Iterator begin, Iterator end, Predicate p, Functor f) {
+    std::vector<std::pair<Iterator, typename std::iterator_traits<Iterator>::value_type>> log;
+    bool error = false;
+    try {
+        for (Iterator it = begin; it != end; ++it) {
+            if (p(*it)) {
+                try {
+                    log.emplace_back(it, *it);
+                } catch (...) {
+                    error |= true;
+                }
+                f(*it);
+            }
+        }
+    } catch (...) {
+        if (!error) {
+            for (const auto& [it, value] : log) {
+                *it = value;
+            }
+        }
+        throw;
+    }
+}
