4af114d7eb27d998ffdd053b463b7d4cb427d204
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-12-13 22:05:30 +0300

    pimpl

--- 14_idioms_cpp/pimpl/ugly.cpp
+++ 14_idioms_cpp/pimpl/ugly.cpp
@@ -11,7 +11,7 @@ class Spline::SplineImpl {
    public:
     SplineImpl(std::vector<double>&& x, std::vector<double>&& y, double a, double b)
         : x_(std::move(x)), y_(std::move(y)) {
-        y2_.resize(x_.size(), 0);
+        y2_.reserve(x_.size());
         switch (mySplineSnd(x_.data(), y_.data(), x_.size(), a, b, y2_.data())) {
             case -1:
                 throw std::bad_alloc();
@@ -36,7 +36,6 @@ class Spline::SplineImpl {
     std::vector<double> x_;
     std::vector<double> y_;
     std::vector<double> y2_;
-    double* buffer_ = new double[4](2);
 };
 
 Spline::Spline(std::vector<double> x, std::vector<double> y, double a, double b)
