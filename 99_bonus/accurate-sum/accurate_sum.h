9ee668169f4a1623dc58f66c876f07791ce25d89
pavelryzhov0639289 <pavelryzhov06@gmail.com>
2024-12-19 16:33:35 +0300

    accurate_sum.h

--- 99_bonus/accurate-sum/accurate_sum.h
+++ 99_bonus/accurate-sum/accurate_sum.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <cstddef>
 #include <vector>
 
 inline long double AccurateSum(const std::vector<long double>& data) {
-    long double c = 0.0;
-    for (auto x : data) {
-        c += x;
+    long double c = data[0];
+    long double d = 0.0;
+    for (size_t i = 1; i < data.size(); ++i) {
+        const long double v1 = c + data[i];
+        const long double v2 = v1 - c;
+        const long double v3 = c - (v1 - v2) + data[i] - v2;
+        c = v1;
+        d += v3;
     }
-    return c;
+    return c + d;
 }
